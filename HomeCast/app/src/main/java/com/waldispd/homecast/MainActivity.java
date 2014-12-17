72ddb88a9fa80c0cd0c0ad49924dbb815c66dc5d
waldii <beispiel444@outlook.com>
2014-12-17 23:03:12 +0100

    Chromecast
    first chromecast integration/implementation

--- HomeCast/app/src/main/java/com/waldispd/homecast/MainActivity.java
+++ HomeCast/app/src/main/java/com/waldispd/homecast/MainActivity.java
@@ -1,30 +1,38 @@
 package com.waldispd.homecast;
 
 import android.app.Activity;
+import android.app.MediaRouteActionProvider;
 import android.content.res.XmlResourceParser;
+//import android.media.MediaRouter;
 import android.os.StrictMode;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.internal.widget.AdapterViewCompat;
-import android.view.Gravity;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.cast.ApplicationMetadata;
+import com.google.android.gms.cast.Cast;
+import com.google.android.gms.cast.CastDevice;
+import com.google.android.gms.cast.CastMediaControlIntent;
+import com.google.android.gms.cast.MediaInfo;
+import com.google.android.gms.cast.MediaMetadata;
+import com.google.android.gms.cast.MediaStatus;
+import com.google.android.gms.cast.RemoteMediaPlayer;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -64,6 +72,19 @@ public class MainActivity extends ActionBarActivity
     Serie[] series;
     Staffel[] staffelList;
 
+    // ChromeCast \\
+    private MediaRouter mMediaRouter;
+    private MediaRouteSelector mMediaRouteSelector;
+    private MediaRouter.Callback mMediaRouterCallback;
+    private CastDevice mSelectedDevice;
+    private GoogleApiClient mApiClient;
+    private RemoteMediaPlayer mRemoteMediaPlayer;
+    private Cast.Listener mCastClientListener;
+    private boolean mWaitingForReconnect = false;
+    private boolean mApplicationStarted = false;
+    private boolean mVideoIsLoaded;
+    private boolean mIsPlaying;
+
     File xmlFile = new File("\\\\192.168.1.115\\j$\\Serien\\serien.xml");
 
     @Override
@@ -83,6 +104,18 @@ public class MainActivity extends ActionBarActivity
         LoadXmlFile();
 
         XmlParsing();
+
+        InitMediaRouter();
+    }
+
+    private void InitMediaRouter()
+    {
+        mMediaRouter = android.support.v7.media.MediaRouter.getInstance(getApplicationContext());
+        mMediaRouteSelector = new MediaRouteSelector.Builder()
+                .addControlCategory(
+                        CastMediaControlIntent.categoryForCast(getString(R.string.application_id)))
+                .build();
+        mMediaRouterCallback = new MediaRouterCallback();
     }
 
     private String LoadXmlFile()
@@ -91,7 +124,7 @@ public class MainActivity extends ActionBarActivity
         {
             StrictMode.ThreadPolicy tp = StrictMode.ThreadPolicy.LAX;
             StrictMode.setThreadPolicy(tp);
-            String user = "user:password";
+            String user = "user:pw";
             NtlmPasswordAuthentication auth = new NtlmPasswordAuthentication(user);
             String path = "smb://192.168.1.115/Serien/serien.xml"; //192.168.1.115
             SmbFile sFile = new SmbFile(path, auth);
@@ -223,8 +256,51 @@ public class MainActivity extends ActionBarActivity
 
     public void LoadEpisode(Episode episode)
     {
+        if( !mVideoIsLoaded )
+            startVideo(episode);
+        else
+            controlVideo();
+    }
+
+    public void startVideo(Episode episode)
+    {
         Toast toast = Toast.makeText(getApplicationContext(), "Episode ::" + String.valueOf(episode.number), Toast.LENGTH_SHORT);
         toast.show();
+
+        MediaMetadata mediaMetadata = new MediaMetadata( MediaMetadata.MEDIA_TYPE_MOVIE );
+        mediaMetadata.putString( MediaMetadata.KEY_TITLE, getString(episode.number) );
+
+        MediaInfo mediaInfo = new MediaInfo.Builder( episode.path)
+                .setContentType( getString( R.string.content_type_mp4 ) )
+                .setStreamType( MediaInfo.STREAM_TYPE_BUFFERED )
+                .setMetadata( mediaMetadata )
+                .build();
+        try {
+            mRemoteMediaPlayer.load( mApiClient, mediaInfo, true )
+                    .setResultCallback( new ResultCallback<RemoteMediaPlayer.MediaChannelResult>() {
+                        @Override
+                        public void onResult( RemoteMediaPlayer.MediaChannelResult mediaChannelResult ) {
+                            if( mediaChannelResult.getStatus().isSuccess() ) {
+                                mVideoIsLoaded = true;
+                                //TODO: mButton.setText( getString( R.string.pause_video ) );
+                            }
+                        }
+                    } );
+        } catch( Exception e ) {
+        }
+    }
+
+    private void controlVideo() {
+        if( mRemoteMediaPlayer == null || !mVideoIsLoaded )
+            return;
+
+        if( mIsPlaying ) {
+            mRemoteMediaPlayer.pause( mApiClient );
+            //mButton.setText( getString( R.string.resume_video ) );
+        } else {
+            mRemoteMediaPlayer.play( mApiClient );
+            //mButton.setText( getString( R.string.pause_video ) );
+        }
     }
 
     private void LoadStaffelFragment(Serie serie)
@@ -271,10 +347,31 @@ public class MainActivity extends ActionBarActivity
             // if the drawer is not showing. Otherwise, let the drawer
             // decide what to show in the action bar.
             getMenuInflater().inflate(R.menu.main, menu);
+            MenuItem mediaRouteMenuItem = menu.findItem(R.id.media_route_menu_item);
+            android.support.v7.app.MediaRouteActionProvider mediaRouteActionProvider  = (android.support.v7.app.MediaRouteActionProvider)
+                    MenuItemCompat.getActionProvider(mediaRouteMenuItem);
+            mediaRouteActionProvider.setRouteSelector(mMediaRouteSelector);
             restoreActionBar();
             return true;
         }
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        // Start media router discovery
+        mMediaRouter.addCallback( mMediaRouteSelector, mMediaRouterCallback, MediaRouter.CALLBACK_FLAG_PERFORM_ACTIVE_SCAN );
+    }
+
+    @Override
+    protected void onPause() {
+        if ( isFinishing() ) {
+            // End media router discovery
+            mMediaRouter.removeCallback( mMediaRouterCallback );
+        }
+        super.onPause();
     }
 
     @Override
@@ -290,6 +387,165 @@ public class MainActivity extends ActionBarActivity
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class MediaRouterCallback extends MediaRouter.Callback {
+
+        @Override
+        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo info) {
+            initCastClientListener();
+            initRemoteMediaPlayer();
+
+            mSelectedDevice = CastDevice.getFromBundle( info.getExtras() );
+
+            launchReceiver();
+        }
+
+        @Override
+        public void onRouteUnselected( MediaRouter router, MediaRouter.RouteInfo info ) {
+            teardown();
+            mSelectedDevice = null;
+            //TODO: mButton.setText( getString( R.string.play_video ) );
+            mVideoIsLoaded = false;
+        }
+
+        private void initCastClientListener() {
+            mCastClientListener = new Cast.Listener() {
+                @Override
+                public void onApplicationStatusChanged() {
+                }
+
+                @Override
+                public void onVolumeChanged() {
+                }
+
+                @Override
+                public void onApplicationDisconnected( int statusCode ) {
+                    teardown();
+                }
+            };
+        }
+
+        private void launchReceiver() {
+            Cast.CastOptions.Builder apiOptionsBuilder = Cast.CastOptions
+                    .builder( mSelectedDevice, mCastClientListener );
+
+            ConnectionCallbacks mConnectionCallbacks = new ConnectionCallbacks();
+            ConnectionFailedListener mConnectionFailedListener = new ConnectionFailedListener();
+            mApiClient = new GoogleApiClient.Builder(getApplicationContext())
+                    .addApi( Cast.API, apiOptionsBuilder.build() )
+                    .addConnectionCallbacks( mConnectionCallbacks )
+                    .addOnConnectionFailedListener( mConnectionFailedListener )
+                    .build();
+
+            mApiClient.connect();
+        }
+
+        private void initRemoteMediaPlayer() {
+            mRemoteMediaPlayer = new RemoteMediaPlayer();
+            mRemoteMediaPlayer.setOnStatusUpdatedListener( new RemoteMediaPlayer.OnStatusUpdatedListener() {
+                @Override
+                public void onStatusUpdated() {
+                    MediaStatus mediaStatus = mRemoteMediaPlayer.getMediaStatus();
+                    mIsPlaying = mediaStatus.getPlayerState() == MediaStatus.PLAYER_STATE_PLAYING;
+                }
+            });
+
+            mRemoteMediaPlayer.setOnMetadataUpdatedListener( new RemoteMediaPlayer.OnMetadataUpdatedListener() {
+                @Override
+                public void onMetadataUpdated() {
+                }
+            });
+        }
+    }
+
+    private class ConnectionCallbacks implements GoogleApiClient.ConnectionCallbacks {
+
+        @Override
+        public void onConnected( Bundle hint ) {
+            if( mWaitingForReconnect ) {
+                mWaitingForReconnect = false;
+                reconnectChannels( hint );
+            } else {
+                try {
+                    Cast.CastApi.launchApplication( mApiClient, getString( R.string.application_id ), false )
+                            .setResultCallback(
+                                    new ResultCallback<Cast.ApplicationConnectionResult>() {
+                                        @Override
+                                        public void onResult(
+                                                Cast.ApplicationConnectionResult applicationConnectionResult) {
+                                            Status status = applicationConnectionResult.getStatus();
+                                            if( status.isSuccess() ) {
+                                                //Values that can be useful for storing/logic
+                                                ApplicationMetadata applicationMetadata =
+                                                        applicationConnectionResult.getApplicationMetadata();
+                                                String sessionId =
+                                                        applicationConnectionResult.getSessionId();
+                                                String applicationStatus =
+                                                        applicationConnectionResult.getApplicationStatus();
+                                                boolean wasLaunched =
+                                                        applicationConnectionResult.getWasLaunched();
+
+                                                mApplicationStarted = true;
+                                                reconnectChannels( null );
+                                            }
+                                        }
+                                    }
+                            );
+                } catch ( Exception e ) {
+
+                }
+            }
+        }
+
+        private void reconnectChannels( Bundle hint ) {
+            if( ( hint != null ) && hint.getBoolean( Cast.EXTRA_APP_NO_LONGER_RUNNING ) ) {
+                //Log.e( TAG, "App is no longer running" );
+                teardown();
+            } else {
+                try {
+                    Cast.CastApi.setMessageReceivedCallbacks( mApiClient, mRemoteMediaPlayer.getNamespace(), mRemoteMediaPlayer );
+                } catch( IOException e ) {
+                    //Log.e( TAG, "Exception while creating media channel ", e );
+                } catch( NullPointerException e ) {
+                    //Log.e( TAG, "Something wasn't reinitialized for reconnectChannels" );
+                }
+            }
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+            mWaitingForReconnect = true;
+        }
+    }
+
+    private class ConnectionFailedListener implements GoogleApiClient.OnConnectionFailedListener {
+        @Override
+        public void onConnectionFailed( ConnectionResult connectionResult ) {
+            teardown();
+        }
+    }
+
+    private void teardown() {
+        if( mApiClient != null ) {
+            if( mApplicationStarted ) {
+                try {
+                    Cast.CastApi.stopApplication( mApiClient );
+                    if( mRemoteMediaPlayer != null ) {
+                        Cast.CastApi.removeMessageReceivedCallbacks( mApiClient, mRemoteMediaPlayer.getNamespace() );
+                        mRemoteMediaPlayer = null;
+                    }
+                } catch( IOException e ) {
+                    //Log.e( TAG, "Exception while removing application " + e );
+                }
+                mApplicationStarted = false;
+            }
+            if( mApiClient.isConnected() )
+                mApiClient.disconnect();
+            mApiClient = null;
+        }
+        mSelectedDevice = null;
+        mVideoIsLoaded = false;
     }
 
     /**
